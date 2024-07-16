5429d6692236338126bb05ce447581546aa40ff5
Luiz Matheus <luiz.matheus@ufrpe.br>
2024-07-16 18:46:56 -0300

    first commit

---
+++ torch_version.py
@@ -0,0 +1,9 @@
+import torch
+import torchvision
+import torchaudio
+
+# print(f'Torch: {torch.__version__}')
+# print(f'Torchvision: {torchvision.__version__}')
+# print(f'Torchaudio: {torchaudio.__version__}')
+
+print(f'Availability: {torch.cuda.is_available()}')
